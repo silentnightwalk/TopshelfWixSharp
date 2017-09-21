3b288127748a185062e4b54414a1aed8f942e68f
silentnightwalk <andrey.uni@gmail.com>
2017-09-21 19:51:16 +0400

    first commit

---
+++ Installer/Setup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using WixSharp;
+using WixSharp.CommonTasks;
+
+namespace Installer
+{
+    class Setup
+    {
+        static void Main(string[] args)
+        {
+            Compiler.WixLocation = @"..\packages\WixSharp.wix.bin.3.11.0\tools\bin";
+            var project = new ManagedProject("TopshelfWixSharp"
+                , new Dir(
+                    $@"{Environment.ExpandEnvironmentVariables("%programfiles(x86)%")}\TopshelfWixSharp"
+                    , new File($@"..\TopshelfWixSharp\bin\Debug\TopshelfWixSharp.exe")
+                    , new File($@"..\TopshelfWixSharp\bin\Debug\Topshelf.dll")
+                    )
+                );
+            project.BeforeInstall += Project_BeforeInstall;
+            project.AfterInstall += Project_AfterInstall;
+            Compiler.BuildMsi(project);
+        }
+
+        private static void Project_BeforeInstall(SetupEventArgs e)
+        {
+            if (e.IsUninstalling)
+            {
+                Tasks.StopService("TopshelfWixSharp");
+
+                Process proc = new Process();
+                proc.StartInfo.FileName = $@"{Environment.ExpandEnvironmentVariables("%programfiles(x86)%")}\TopshelfWixSharp\TopshelfWixSharp.exe";
+                proc.StartInfo.Arguments = "uninstall";
+                proc.StartInfo.Verb = "runas";
+                proc.Start();
+            }
+        }
+
+        private static void Project_AfterInstall(SetupEventArgs e)
+        {
+            if (e.IsInstalling)
+            {
+                Process proc = new Process();
+                proc.StartInfo.FileName = $@"{Environment.ExpandEnvironmentVariables("%programfiles(x86)%")}\TopshelfWixSharp\TopshelfWixSharp.exe";
+                proc.StartInfo.Arguments = "install start";
+                proc.StartInfo.Verb = "runas";
+                proc.Start();
+            }
+        }
+    }
+}
---
+++ TopshelfWixSharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using Topshelf;
+
+namespace TopshelfWixSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>                                
+            {
+                x.Service<TownCrier>(s =>                       
+                {
+                    s.ConstructUsing(name => new TownCrier());  
+                    s.WhenStarted(tc => tc.Start());            
+                    s.WhenStopped(tc => tc.Stop());             
+                });
+                x.RunAsLocalSystem();                           
+
+                x.SetDescription("----------------");       
+                x.SetDisplayName("TopshelfWixSharp");                      
+                x.SetServiceName("TopshelfWixSharp");                      
+            });
+            Console.ReadLine();                                                       
+        }
+
+        public class TownCrier
+        {
+            readonly Timer _timer;
+            public TownCrier()
+            {
+                _timer = new Timer(1000) { AutoReset = true };
+                _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} and all is well", DateTime.Now);
+            }
+            public void Start() { _timer.Start(); }
+            public void Stop() { _timer.Stop(); }
+        }
+    }
+}
